4062da34d3c625f7ea546dbb2b9e62c8c3171872
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-04-01 20:08:24 +0200

    det

--- algebra/mat_properties.hpp
+++ algebra/mat_properties.hpp
@@ -9,6 +9,32 @@ double mat<T>::det() const
 	if (rows != cols)
 		throw std::string("Matrice non quadrata");
 
+	switch (rows)
+	{
+	case 1:
+		return matrix[0][0];
+		break;
+
+	case 2:
+		return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+		break;
+
+	case 3:
+		double p = 1;
+		double s = 0;
+		for (size_t i = 0; i < 3; i++)
+		{
+			for (size_t j = i; j < 3; j++)
+			{
+				p *= matrix[j % 3][j % 3];
+			}
+		}
+		break;
+
+	default:
+		break;
+	}
+
 	return 0.0;
 }
 
