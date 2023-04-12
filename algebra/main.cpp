a9c918eb9d61823f6626218cb5b17de94103cdbb
FedericoBustaffa <kiko.bustaffa@gmail.com>
2023-04-12 16:54:23 +0200

    fix

--- algebra/main.cpp
+++ algebra/main.cpp
@@ -7,15 +7,7 @@ int main()
 	mat<double> a = mat<double>::identity(3);
 	mat<double> b(4, 8);
 
-	try
-	{
-		std::cout << a.det() << std::endl;
-		std::cout << b.det() << std::endl;
-	}
-	catch (std::string e)
-	{
-		std::cout << e << std::endl;
-	}
+    std::cout << a << std::endl;
 
 	return 0;
 }
